1ffecea9ec03fd3b4746069dd2464484bd2afe91
Santosh Pangeni <sntshpangeni@gmail.com>
2021-06-26 07:27:25 +0545

    First one

---
+++ src/Components/modalContent.js
@@ -0,0 +1,131 @@
+var modalContent = {
+    about: {
+        title: 'About Me',
+        content: [{
+                id: 1,
+                desc: 'Hi, I am a Computer Engineering student.'
+            },
+            {
+                id: 2,
+                desc: "I specialize in web and app design, front and back end development, user experience, and creating identities and branding."
+            },
+            {
+                id: 3,
+                desc: "For all inquiries and opportunities, contact me if you'd like to work together on your next project."
+            }
+        ]
+    },
+    tech: {
+        title: 'Tech Stack',
+        content: [{
+                id: 1,
+                name: 'JavaScript',
+                url: '/images/javascript.svg'
+            },
+            {
+                id: 2,
+                name: 'React',
+                url: '/images/react.svg'
+            },
+            {
+                id: 3,
+                name: 'Python',
+                url: '/images/python.svg'
+            },
+            {
+                id: 4,
+                name: 'Node',
+                url: '/images/nodejs.svg'
+            },
+            {
+                id: 5,
+                name: 'C#',
+                url: '/images/csharp.svg'
+            },
+            {
+                id: 6,
+                name: 'Django',
+                url: '/images/djangologo.svg'
+            },
+            {
+                id: 7,
+                name: 'Css',
+                url: '/images/css3.svg'
+            },
+            {
+                id: 8,
+                name: 'HTML',
+                url: '/images/html5.svg'
+            },
+            {
+                id: 9,
+                name: 'MongoDB',
+                url: '/images/mongodb.svg'
+            }
+        ]
+    },
+    portfolio: {
+        title: 'Portfolio',
+        content: [{
+                id: 1,
+                project: 'shopping-app',
+                description: 'E-commerce Api',
+                img: '/images/javascript.svg',
+                repoLink: 'https://github.com/Santosh-p23/shopping-app-v2'
+            },
+            {
+                id: 2,
+                project: '2D fighting game',
+                description: 'With SFML and C++',
+                img: '/images/bison.svg',
+                repoLink: 'https://github.com/Santosh-p23/tekken-proj-sfml'
+            },
+            {
+                id: 3,
+                project: ' 3D FPS Game',
+                description: 'Unity C# game',
+                img: '/images/swordNshield.svg',
+                repoLink: 'https://github.com/Santosh-p23/FirstPersonShooter-Jungle-Fury'
+            },
+            {
+                id: 4,
+                project: 'Social Media API',
+                description: 'Node based API',
+                img: '/images/nodejs.svg',
+                repoLink: 'https://github.com/Santosh-p23/SocialMedia-Api'
+            },
+            {
+                id: 5,
+                project: 'For more...',
+                description: 'Please visit my Github Profile',
+                img: '/images/more.svg',
+                repoLink: 'https://github.com/Santosh-p23?tab=repositories'
+            }
+        ]
+    },
+    contact: {
+        title: 'Contact Me',
+        content: [{
+                title: 'Email',
+                url: 'mailto:sntshpangeni@gmail.com',
+                source: '/images/email.svg'
+            },
+            {
+                title: 'Github',
+                url: 'https://github.com/Santosh-p23',
+                source: '/images/github.svg'
+            },
+            {
+                title: 'LinkedIn',
+                url: 'https://www.linkedin.com/in/santosh-pangeni-4b9b161a6',
+                source: '/images/linkedin.svg'
+            }
+        ]
+    },
+    blog: {
+        title: "COMING SOON !",
+        content: ''
+    }
+};
+
+export default modalContent;
